81ee8d5c3c77be9f83118dab6aa640475cd8e2be
Payne <wuzhipeng1289690157@gamil.com>
2021-06-03 01:12:34 +0800

    add eval

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JsHookScript
+
 JsHookScript, All the hook scripts I know
+
+## Hook Everthing of eval
+
+- [x] Hook Everthing of eval
---
+++ eval.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name             Hook Everthing of eval
+// @namespace    http://tampermonkey.net/
+// @version          0.1
+// @description   Try to Hook Everthing eval
+// @author          Payne
+// @match          *
+// @icon             https://www.google.com/s2/favicons?domain=tampermonkey.net
+// @grant        none
+// @run-at      document-start
+// ==/UserScript==
+
+(function() {
+    alert('Start Hooking ...');
+    // Building replica function objects,  Prepare the hook
+    var eval_new = eval;
+    //rewrite function to hook;
+    eval = function(val) {
+        debugger;
+        console.log('val :', val);
+        return eval_new(val)
+ };
+    // Disguise the prototype
+    eval.toString = function(){
+        return "function eval() { [native code] }"
+ };
+    eval.length = 1;
+})();
